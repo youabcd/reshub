58c0195917b2ca4fc91211d235aec7dd94d88113
youabcd <63175220+youabcd@users.noreply.github.com>
2020-10-29 18:09:20 +0800

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -9,11 +9,11 @@
           </div>
           <el-menu :default-active="nowActive">
             <el-menu-item index="1" @click="nowActive='1'">
-              <i class="el-icon-menu"></i>
+              <span style="">●</span>
               <span slot="title">系统消息</span>
             </el-menu-item>
             <el-menu-item index="2" @click="nowActive='2'">
-              <i class="el-icon-menu"></i>
+              <span style="">●</span>
               <span slot="title">我的消息</span>
             </el-menu-item>
           </el-menu>
@@ -22,23 +22,17 @@
         <el-container>
           <el-main>
             <div v-if="nowActive=='1'">
-              <div style="box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);">
+              <div style="box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);border-radius: 8px;">
                 <van-row></van-row>
                 <van-row>
-                  <van-col span="18" style="margin-left:15px;margin-bottom: 10px;margin-top: 10px;text-align:left;font-size: 22px;color: lightgrey">
+                  <van-col span="18" style="margin-left:15px;margin-bottom: 10px;margin-top: 10px;text-align:left;font-size: 18px;">
                     系统消息
                   </van-col>
-                  <van-col span="2">
-                    一键已读
-                  </van-col>
-                  <van-col span="2">
-                    一键清空
-                  </van-col>
-                  <van-col span="2"></van-col>
+                  <van-col span="6"></van-col>
                 </van-row>
                 <van-row></van-row>
               </div>
-              <div v-for="(item,index) in systemMessage" style="margin-top: 15px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);">
+              <div v-for="(item,index) in systemMessage" style="margin-top: 15px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);border-radius: 5px;">
               <van-row></van-row>
               <van-row style="margin-left:15px;margin-top: 10px;">
                 <van-col span="12" style="font-size: 20px;text-align: left;">
@@ -58,8 +52,32 @@
 
             <!--私信-->
             <div v-if="nowActive=='2'">
-              <el-container>
-                <el-header></el-header>
+              <div style="box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);border-radius: 8px;">
+                <van-row></van-row>
+                <van-row>
+                  <van-col span="18" style="margin-left:15px;margin-bottom: 10px;margin-top: 10px;text-align:left;font-size: 18px;">
+                    我的消息
+                  </van-col>
+                  <van-col span="6"></van-col>
+                </van-row>
+                <van-row></van-row>
+              </div>
+              <el-container style="height: 590px;">
+                <el-aside width="220px" style="">
+                  <div style="margin-top: 10px;font-size: 20px;">
+                    消息中心
+                  </div>
+                  <div>
+                    <van-cell v-for="(item,index) in recentMessage" :key="index" clickable @click="openChats(item)">
+                      {{item.friendName}}
+                    </van-cell>
+                  </div>
+                </el-aside>
+                <el-main>
+                  <div class="message2">
+                    大家好，我是小胡<br>个人主页：hlz.space<br>个人服务网站：www.loveconvert.com
+                  </div>
+                </el-main>
               </el-container>
             </div>
           </el-main>
@@ -96,14 +114,78 @@
               {title:'message2',texts:'dfhkjdhfjhjhjkfhjghadfkj gj hk shdkjh ahkh ah sahkjsah skjhsjgh kshjhsdkjh khjkdhjkh gkjhdgkjhdfjkgh djkkj',sendTime:'2020/10/7 19:53'},
             ],
 
+            recentMessage:[
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+              {friendName:'youabcd'},
+            ],
+            chats:[
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:11'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:12'},
+              {sender:'aaa',msg:'吃饭',sendTime:'2020/10/29 17:13'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:14'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:15'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:16'},
+              {sender:'aaa',msg:'吃饭',sendTime:'2020/10/29 17:17'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:18'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:19'},
+              {sender:'aaa',msg:'吃饭',sendTime:'2020/10/29 17:20'},
+              {sender:'ccc',msg:'吃饭',sendTime:'2020/10/29 17:21'},
+              {sender:'aaa',msg:'吃饭',sendTime:'2020/10/29 17:22'},
+            ]
           }
       },
       components:{
         TopBar
       },
+      methods:{
+        openChats(item){
+
+        },
+      }
     }
 </script>
 
 <style>
+  .message2 {
+    float:left;
+    width: 200px;
+    height: 80px;
+    margin: 50px;
+    background-color: skyblue;
+    border-bottom-color: skyblue;
+    color: #fff;
+    font-size: 12px;
+    line-height: 18px;
+    padding: 5px 12px 5px 12px;
+    box-sizing: border-box;
+    border-radius: 6px;
+    position: relative;
+    word-break: break-all;
+  }
+  .message2::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: -5px;
+    width: 10px;
+    height: 10px;
+    margin-top: -10px;
+    background: inherit;
+    transform: rotate(45deg);
+  }
 
 </style>
