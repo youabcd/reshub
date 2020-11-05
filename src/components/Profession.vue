e99efb96d1f750cf7efb5f3faad243ce1110fb00
Thomas-yz <aladun@yeah.net>
2020-11-05 20:21:33 +0800

    yz_update_profession

--- src/components/Profession.vue
+++ src/components/Profession.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>
+  <div style="overflow-y:scroll;">
     <TopBar></TopBar>
-    具体专业模块
+    <div style="background-color: white;border-width: 1px;border-color: #666666;margin-left: 15%;margin-top: 2%;width: 70%;float:left;">
+      <el-page-header @back="goBack" content="全部学科与领域"></el-page-header>
+      <el-card class="box-card" style="float: left;">
+        <div slot="header" class="clearfix">
+          <span>计算机</span>
+        </div>
+        <div v-for="o in 5" :key="o" class="text item">
+          {{'具体领域 ' + o }}
+        </div>
+      </el-card>
+      <el-card class="box-card" style="float: right;">
+        <div slot="header" class="clearfix">
+          <span>医学</span>
+        </div>
+        <div v-for="o in 5" :key="o" class="text item">
+          {{'具体领域 ' + o }}
+        </div>
+      </el-card>
+      <el-card class="box-card" style="float: left;">
+        <div slot="header" class="clearfix">
+          <span>软件</span>
+        </div>
+        <div v-for="o in 5" :key="o" class="text item">
+          {{'具体领域 ' + o }}
+        </div>
+      </el-card>
+      <el-card class="box-card" style="float: right;">
+        <div slot="header" class="clearfix">
+          <span>哲学</span>
+        </div>
+        <div v-for="o in 5" :key="o" class="text item">
+          {{'具体领域 ' + o }}
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -17,5 +51,28 @@
 </script>
 
 <style scoped>
-
+  .text {
+      font-size: 14px;
+    }
+  
+    .item {
+      margin-bottom: 18px;
+    }
+  
+    .clearfix:before,
+    .clearfix:after {
+      display: table;
+      content: "";
+    }
+    .clearfix:after {
+      clear: both
+    }
+  
+    .box-card {
+      margin-left: 5px;
+      margin-right: 5px;
+      margin-top: 5px;
+      margin-bottom: 5px;
+      width: 49%;
+    }
 </style>
