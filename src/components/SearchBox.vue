722a99e6cc8ce2f5b54d8dceeb7fdc400c98b9c3
youabcd <63175220+youabcd@users.noreply.github.com>
2020-11-07 10:22:39 +0800

    czt

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -76,6 +76,8 @@
 </template>
 
 <script>
+    import axios from 'axios';
+    import baseUrl from './baseUrl'
     export default {
       name: "SearchBox",
       data(){
@@ -146,12 +148,18 @@
           };
         },
         loadAll() {
-          this.recommend=[{ "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+          let _this=this;
+          axios.get(baseUrl+'/recommend')
+          .then(function (response) {
+            console.log(response);
+            _this.recommend=response.data;
+          })
+          /*this.recommend=[{ "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
             { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
             { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
             { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
             { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
-            { "value": "贡茶", "address": "上海市长宁区金钟路633号" }];
+            { "value": "贡茶", "address": "上海市长宁区金钟路633号" }];*/
           return this.recommend;
         },
         handleSelect(item) {
---
+++ src/components/baseUrl.js
@@ -0,0 +1,3 @@
+let baseUrl = 'https://www.easy-mock.com/mock/5fa4b95160b9b97b2c3994de';  // 测试环境  测试时使用此路径
+//let baseUrl = '';  // 生产环境  打包前改成此路径
+export default baseUrl;
