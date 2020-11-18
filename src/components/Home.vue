d210acb316de4d4381fda30b65228d6f72c9b154
youabcd <63175220+youabcd@users.noreply.github.com>
2020-11-18 15:32:19 +0800

    searchbox

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,12 +20,26 @@
     name: "Home",
     data() {
       return {
+        backImg:[
+          url('../assets/bing01.jpg'),
+          url('../assets/bing02.jpg'),
+          '../assets/bing03.jpg',
+          '../assets/bing04.jpg',
+          '../assets/bing05.jpg',
+          '../assets/bing06.jpg',
+          '../assets/bing07.jpg',
+          '../assets/bing08.jpg',
+        ],
       }
     },
     methods:{
     },
     components: {
       TopBar,SearchBox,
+    },
+    created() {
+      //var index =parseInt(Math.random()*7);
+      //document.getElementById("BackgroundArea").style.backgroundImage=this.backImg[0];
     },
   }
 </script>
@@ -35,8 +49,9 @@
     width:100%;
     height:100%;  /**宽高100%是为了图片铺满屏幕 */
     z-index:-1;
-  //opacity:0.9;
-    background-image: url("../assets/background01.jpg");
     position: fixed;
+    background-image: url("../assets/bing02.jpg");
+    background-size: 100%;
+    background-repeat: no-repeat;
   }
 </style>
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -3,73 +3,75 @@
     <!--搜索框样式-->
     <div>
       <div style="margin-top: 60px;width: 70%;margin-left: 15%" @keyup.enter="search">
-        <van-row>
+        <van-row v-for="(item,index) in searchKey" :key="index" style="margin-top: 15px;">
           <van-col span="22">
-            <el-autocomplete placeholder="请输入内容" v-model="keyWords"
+            <el-autocomplete v-if="index==0" placeholder="请输入内容" v-model="item.words"
                              :fetch-suggestions="querySearch" @select="handleSelect"
                              style="font-size: 20px;width: 100%;" class="input-with-select">
-              <el-select v-model="select" slot="prepend" style="font-size: 15px">
-                <el-option label="搜索全部" value="1"></el-option>
-                <el-option label="搜索资料" value="2"></el-option>
-                <el-option label="搜索学者" value="3"></el-option>
+              <el-select v-if="index>0" v-model="item.boolType" slot="prepend" style="font-size: 15px">
+                <el-option label="AND" value="1"></el-option>
+                <el-option label="OR" value="2"></el-option>
+                <el-option label="NOT" value="3"></el-option>
               </el-select>
-              <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+              <el-select slot="append" v-model="item.type" style="font-size: 15px">
+                <el-option label="主题" value="1"></el-option>
+                <el-option label="标题" value="2"></el-option>
+                <el-option label="作者" value="3"></el-option>
+                <el-option label="关键词" value="4"></el-option>
+                <el-option label="摘要" value="5"></el-option>
+                <el-option label="全文" value="6"></el-option>
+              </el-select>
             </el-autocomplete>
+            <el-input v-if="index>0" placeholder="" v-model="item.words"
+                             style="font-size: 20px;width: 100%;" class="input-with-select">
+              <el-select v-if="index>0" v-model="item.boolType" slot="prepend" style="font-size: 15px">
+                <el-option label="AND" value="1"></el-option>
+                <el-option label="OR" value="2"></el-option>
+                <el-option label="NOT" value="3"></el-option>
+              </el-select>
+              <el-select slot="append" v-model="item.type" style="font-size: 15px">
+                <el-option label="主题" value="1"></el-option>
+                <el-option label="标题" value="2"></el-option>
+                <el-option label="作者" value="3"></el-option>
+                <el-option label="关键词" value="4"></el-option>
+                <el-option label="摘要" value="5"></el-option>
+                <el-option label="全文" value="6"></el-option>
+              </el-select>
+            </el-input>
           </van-col>
           <van-col span="2">
-            <van-row>
-              &nbsp;
-            </van-row>
-            <van-row>
-              <el-link type="" @click="qualitySearch">>>高级检索</el-link>
-            </van-row>
+            <van-row></van-row>
+            <el-link v-if="index>0" type="" @click="declineSearchBox(item,index)" style="margin-top: 5px;text-align: left;font-size: 30px;">
+              <el-tooltip class="item" effect="light" content="删除该行" placement="right">
+                <i class="el-icon-remove-outline"></i>
+              </el-tooltip>
+            </el-link>
           </van-col>
         </van-row>
       </div>
 
-      <!--高级搜索弹框-->
-      <div>
-        <van-popup v-model="show" round closeable style="height: 550px;width: 550px;">
-          <el-form ref="form" :model="form" :rules="rules" label-width="110px" style="margin-top: 35px;width: 85%;margin-left: 5%">
-            <el-form-item label="检索词" prop="keyWords">
-              <el-input v-model="form.keyWords"></el-input>
-            </el-form-item>
-            <el-form-item label="检索词出现位置">
-              <el-radio-group v-model="form.region">
-                <el-radio-button label="文章任意位置"></el-radio-button>
-                <el-radio-button label="文章标题"></el-radio-button>
-                <el-radio-button label="文章摘要"></el-radio-button>
-              </el-radio-group>
-            </el-form-item>
-            <el-form-item label="发表时间范围">
-              <el-col :span="11">
-                <el-date-picker type="date" placeholder="选择开始日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-              </el-col>
-              <el-col class="line" :span="2">-</el-col>
-              <el-col :span="11">
-                <el-date-picker type="date" placeholder="选择结束日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="专业方向">
-              <el-input v-model="form.profession"></el-input>
-            </el-form-item>
-            <el-form-item label="作者">
-              <el-input v-model="form.author"></el-input>
-            </el-form-item>
-            <el-form-item label="机构">
-              <el-input v-model="form.institute"></el-input>
-            </el-form-item>
-            <el-form-item label="文章所用语言">
-              <el-radio-group v-model="form.language">
-                <el-radio label="1">任意</el-radio>
-                <el-radio label="2">中文</el-radio>
-                <el-radio label="3">英文</el-radio>
-              </el-radio-group>
-            </el-form-item>
-          </el-form>
-          <el-button type="primary" @click="onSubmit('form')">搜索</el-button>
-        </van-popup>
+      <div style="margin-top: 15px;width: 70%;margin-left: 15%">
+        <van-row>
+          <van-col span="2" style="font-size: 18px;margin-top: 5px;">时间范围</van-col>
+          <van-col span="5">
+            <el-date-picker type="date" placeholder="选择开始日期" v-model="dataStart" style="width: 100%;"></el-date-picker>
+          </van-col>
+          <van-col span="5">
+            <el-date-picker type="date" placeholder="选择结束日期" v-model="dataEnd" style="width: 100%;"></el-date-picker>
+          </van-col>
+          <van-col span="6"></van-col>
+          <van-col span="6" style="text-align: left;">
+            <span>
+              <el-link type="" @click="addSearchBox"><i class="el-icon-plus"></i> 添加行</el-link>
+            </span>
+            <span>&nbsp|&nbsp</span>
+            <span>
+              <el-link type="" @click="cleanSearchBox">重设</el-link>
+            </span>
+          </van-col>
+        </van-row>
       </div>
+
     </div>
 
   </div>
@@ -84,28 +86,22 @@
       return{
         //搜索框部分数据
         keyWords:'',
-        select:'1',
+        /*boolType {1:AND ; 2:OR ; 3:NOT}
+        type {1：主题；2：标题；3：作者；4：关键词；5：摘要；6：全文}
+        */
+        searchKey:[
+          {type:'1',words:'',isFuzzy:false,boolType:'1'},
+
+        ],
+        dataStart:'',
+        dataEnd:'',
         recommend: [],
-        show:false,
-        form: {
-          keyWords:'',//关键词
-          region: '文章任意位置',//出现位置
-          date1: '',//搜索时间开始
-          date2: '',//搜索实际结束
-          profession:'',
-          author:'',//作者名字
-          institute:'',//机构名称
-          language:'1',//语种
-        },
-        rules:{
-          keyWords:[{required:true,message:'请输入检索词',trigger:'blur'}]
-        },
       }
     },
     methods:{
       //搜索框部分函数
       search(){
-        if(this.keyWords.length==0){
+        if(this.searchKey[0].words.length==0){
           this.$message({
             message: '请输入搜索关键词',
             type: 'error'
@@ -123,9 +119,25 @@
           }
         }
       },//搜索
-      qualitySearch(){
-        this.show=true;
-      },//打开高级搜索弹框
+      //添加行
+      addSearchBox(){
+        var a={type:'1',words:'',isFuzzy:false,boolType:'1'};
+        this.searchKey.push(a);
+        if(this.searchKey.length>10){
+          this.$message({
+            message: '不建议再继续增加搜索框,可能会导致搜不到结果',
+            type: 'warning'
+          });
+        }
+      },
+      //重设
+      cleanSearchBox(){
+        this.searchKey.splice(1,this.searchKey.length-1);
+      },
+      //删除行
+      declineSearchBox(item,index){
+        this.searchKey.splice(index,1);
+      },
       onSubmit(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
