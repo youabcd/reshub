1b6b9f89eca5ba69b22394f4a271926fcba144d4
蒋沛宇 <sanz68070@gmail.com>
2020-12-19 19:56:32 +0800

    Update Administrator.vue

--- src/components/Administrator.vue
+++ src/components/Administrator.vue
@@ -214,13 +214,6 @@
                 </div>
               </template>
             </el-table-column>
-            <!--<el-table-column
-              label="操作"
-              width="100">
-              <template slot-scope="scope">
-                <el-button @click="view(scope.$index)" type="text" size="small">查看</el-button>
-              </template>
-            </el-table-column>-->
           </el-table>
         </div>
         <div v-if="menuIndex === '3-1'">
