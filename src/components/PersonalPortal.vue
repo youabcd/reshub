4b3f105fac4cf9f9245f7c21d6eb251a3f4657f9
Thomas-yz <aladun@yeah.net>
2020-12-24 16:57:50 +0800

    Update PersonalPortal.vue

--- src/components/PersonalPortal.vue
+++ src/components/PersonalPortal.vue
@@ -14,12 +14,17 @@
             		<img :src="require('../assets/Head/'+avatar)" alt="学者头像" class="" width="112" height="112">
             	</div>
             	<div style="margin: auto;">
-                <div v-if="!isHave">
-                  <el-button v-if= "isClaimed == true" style="text-align:center;margin-top: 10px;margin-bottom: 10px;" type="primary" @click="gotoCatch01()">我要申诉</el-button>
-                  <el-button v-if= "isClaimed == false" style="text-align:center;margin-top: 10px;margin-bottom: 10px;" type="primary" @click="gotoCatch()">我要认证</el-button></br>
+                <div v-if="userId.length>0">
+                  <div v-if="!isHave">
+                    <el-button v-if= "isClaimed == true" style="text-align:center;margin-top: 10px;margin-bottom: 10px;" type="primary" @click="gotoCatch01()">我要申诉</el-button>
+                    <el-button v-if= "isClaimed == false" style="text-align:center;margin-top: 10px;margin-bottom: 10px;" type="primary" @click="gotoCatch()">我要认证</el-button></br>
+                  </div>
+                  <el-button v-if="this.isFollowing === false" style="width: 70%;" size="mini" type="primary" @click="addConcern()" round plain>关注</el-button>
+                  <el-button v-if="this.isFollowing === true" style="width: 70%;" size="mini" type="primary" @click="cancelConcern()" round plain>取消关注</el-button>
                 </div>
-            	  <el-button v-if="this.isFollowing === false" style="width: 70%;" size="mini" type="primary" @click="addConcern()" round plain>关注</el-button>
-                <el-button v-if="this.isFollowing === true" style="width: 70%;" size="mini" type="primary" @click="cancelConcern()" round plain>取消关注</el-button>
+                <div v-if="!(userId.length>0)">
+                  <el-button style="text-align:center;margin-top: 10px;margin-bottom: 10px;" type="info" disabled>您未登录</el-button>
+                </div>
             	</div>
             </div>"
         		<div class="person_baseinfo">
@@ -224,8 +229,8 @@
           loading:true,
           menuIndex: '0',
           message:'',
-          userId:1,
-          resId:1,
+          userId:'',
+          resId:'',
           avatar:'head00.jpg',
           isHave: true,
           isClaimed: true,
